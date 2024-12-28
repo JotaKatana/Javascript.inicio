f97e21d571f2e0f1c1e65a9c1630ceaa0964561e
Katana <juliocesarvergamini@gmail.com>
2024-12-28 10:14:04 -0300

    control.fluxo

---
+++ controle-de-fluxo.js
@@ -0,0 +1,14 @@
+const numero1 = 100
+const numero2 = 20
+
+
+if (numero1 > 1) {
+    console.log(`Esse numero é maior que 1`)
+}
+
+if (numero1 > 200) {
+    console.log("Esse número é maior que 200")
+}
+    else {
+        console.log("Esse numero é menor que 200!")
+    }
