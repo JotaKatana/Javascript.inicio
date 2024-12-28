d5ccf2021ea49827c33c93a21d4a38d17147b186
Katana <juliocesarvergamini@gmail.com>
2024-12-28 09:56:29 -0300

    first

---
+++ Javascript/arquivo.js
@@ -0,0 +1,20 @@
+// aula introdutória
+const qualquer = null
+console.log(qualquer)
+console.log(teste)
+var teste = "Meu teste"
+let numero = 20
+let numero2 = 300 // inteiro
+const numero_favorito = 5.000001 //real
+const esta_chovendo = false
+
+const nome = "Felipe"
+
+console.log(numero)
+console.log(numero2)
+
+numero = 8
+console.log(numero)
+console.log('Oi', nome, "!")
+console.log(`Oi, ${nome}! Seu numero favorito é: ${numero_favorito}`)
+console.log(`Condição do Clima é chuvoso? Resposta:${esta_chovendo}`)
---
+++ Javascript/veotres.js
@@ -0,0 +1,34 @@
+// ---> Escopo
+{
+    const alunos = [ "Joana", "Cassia", "Kevin" ]   //---> Lista de alunos (Lista/arways)
+    console.log(alunos) //---> Não tem problema eu printar aqui dentro pois é como se fosse uma caixa dentro da outra
+
+    {
+        const condições_de_tempo = ["chuvoso", "ensolarado", "sol entre nuvens"]
+        console.log(condições_de_tempo)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
