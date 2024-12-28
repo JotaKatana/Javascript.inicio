8cf8d9c0baa690ba9c23083d7dc9dabe043de12d
Katana <juliocesarvergamini@gmail.com>
2024-12-28 12:42:46 -0300

    Commit while 1.0

--- while.js
+++ while.js
@@ -14,4 +14,5 @@ console.log({IndiceAtual})
 while (IndiceUsers < IndiceAtual) { //--> Enquanto a variavel que quero inicial for menor que a qtd de vezes que deve rodar, eu quero exibir algo
     console.log("Olá", nome_users[IndiceUsers]) //--> como definimos o rodou vai iniciari assim ("Ola" users [0]), ("Olá" users [0+1])....
     IndiceUsers = IndiceUsers + 1  //--> Cada vez que esse loop fizer ele vai adicionar +1 ao rodou (0+1, 1+1, 2+1...)
+    IndiceUsers += 1 //--> Exatamente a mesma coisa de cima (indice + 1 )
 }                     //--> Estou atribuindo informações, NÃO É IGUALDADE
